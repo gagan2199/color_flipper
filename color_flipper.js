68256e482e651cc0e726aa298b4beb7f893bf34d
Gagan <gagank2199@gmail.com>
2023-08-29 19:50:57 +0530

    color flipper - colors from numbers and alphabets added

--- color_flipper.js
+++ color_flipper.js
@@ -1,37 +1,39 @@
-let container= document.getElementById('container')
-let btn= document.getElementById('btn')
-let colors=[0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E'];
+let container = document.getElementById('container')
+let btn = document.getElementById('btn')
+let colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E'];
 
-let cl=['green','pink','yellow','grey','orange','red','blue','maroon']
+let cl = ['green', 'pink', 'yellow', 'grey', 'orange', 'red', 'blue', 'maroon'];
 
-let v=Math.floor(Math.random()*8);
-let rd,rdc;
+// let uniq;
 
-
-console.log(v);
-
-let uniq;
-
-// for(let i=0;i<6;i++)
-// {
-//     rd=Math.floor(Math.random()*15);
-//      rdc[i]=cl[rd];
-// }
-// container.addEventListener('click',(element)=>
-// {
-
-  
-//     // uniq= cl[Math.floor(Math.random()*8)];
-//     container.style.backgroundColor='white';
-//     // console.log(uniq)
+// btn.addEventListener('click', (element) => {
+//     uniq = cl[Math.floor(Math.random() * 8)];        // from color array
+//     container.style.backgroundColor = uniq;
+//     console.log(uniq)
 // });
 
+let rd;
+let rdc=[];
+                                                           // making color from numbers and alphabets
+for (let i = 0; i < 6; i++) {
+    rd = Math.floor(Math.random() * 15);
+    rdc[i] = colors[rd];
+}
 
-btn.addEventListener('click',(element)=>
-{
-    uniq= cl[Math.floor(Math.random()*8)];
-    container.style.backgroundColor=uniq;
-    console.log(uniq)
+console.log(rdc[0])
+btn.addEventListener('click', (element) => {
+
+    for (let i = 0; i < 6; i++) {
+        rd = Math.floor(Math.random() * 15);
+        rdc[i] = colors[rd];
+    }
+    
+    let str='#'+`${rdc[0]}`+`${rdc[1]}`+`${rdc[2]}`+`${rdc[3]}`+`${rdc[4]}`+`${rdc[5]}`;
+
+    // let str1= rdc[0]+rdc[1]+rdc[2]+rdc[3]+rdc[4]+rdc[5];      //this cannot be used bcz it will add the numbers
+
+    // console.log(str1);
+    console.log(str);
+
+    container.style.backgroundColor =str;
 });
-
-
