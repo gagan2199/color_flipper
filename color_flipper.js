98c88a322223538d8e6984fa8e4c41b78835b38e
Gagan Kumar <114229570+gagan2199@users.noreply.github.com>
2023-12-20 18:22:00 +0530

    Update color_flipper.js

--- color_flipper.js
+++ color_flipper.js
@@ -5,12 +5,12 @@ let colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E'];
 
 let cl = ['green', 'pink', 'yellow', 'grey', 'orange', 'red', 'blue', 'maroon'];
 
-// let uniq;        another method 
+// let uniq;        another method with elements shuffle 
 
 // btn.addEventListener('click', (element) => {
 //     uniq = cl[Math.floor(Math.random() * 8)];        // from color array
 //     container.style.backgroundColor = uniq;
-//     console.log(uniq)
+//     console.log(uniq) 
 // });
 
 let rd;
